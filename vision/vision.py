920828e44f8db14f9baa6d1e932dea6bb508d0a1
EC2 Default User <ec2-user@ip-172-31-80-40.ec2.internal>
2019-11-02 21:31:22 +0000

    Added code to polaraze image

--- vision/vision.py
+++ vision/vision.py
@@ -1,6 +1,19 @@
 #!/usr/local/bin/python3
+from PIL import Image
+from pdf2image import convert_from_path
+import pytesseract
+
+def polarizeImage(image):
+    gray_doc_image = image.convert('L')
+    polarize = lambda pixel: 0 if pixel < 200 else 255
+    bw_doc_image = gray_doc_image.point(polarize, '1') 
+    return bw_doc_image
+
 def main():
-    print("Hello, world! 😊")
+    image = Image.open('testImages/testImg01.JPG')
+    image.show()
+    text = pytesseract.image_to_string(polarizeImage(image))
+    print('\nmain complete\n')
 
 if __name__ == "__main__":
     main()
